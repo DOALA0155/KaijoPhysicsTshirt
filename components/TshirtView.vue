689cb21673a7aec5619e4064b5266ce675695d05
DOALA0155 <kyleshouki@gmail.com>
2020-09-18 23:46:22 +0900

    added fade in animation

---
+++ components/TshirtView.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="tshirt-view">
+    <div class="tshirt-view-content">
+      <div class="tshirt-image" id="image">
+        <img :src="require('~/static/tshirt.png')">
+      </div>
+      <div class="tshirt-description" id="description">
+        <span>海城中高物理部公式Tシャツです。</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      scrollY: 0
+    }
+  },
+  methods: {
+    get_scroll_y: function () {
+      var scroll_top = window.scrollY
+      this.fade_in_out(scroll_top)
+    },
+    fade_in_out: function (scroll_top) {
+      var image = document.getElementById("image")
+      var description = document.getElementById("description")
+
+      if (scroll_top >= (window.innerHeight * 2.99)) {
+        image.style.opacity = 1
+        image.style.marginTop = 0
+        description.style.opacity = 1
+        description.style.marginTop = 0
+      } else {
+        image.style.opacity = 0
+        image.style.marginTop = "2.5vw"
+        description.style.opacity = 0
+        description.style.marginTop = "5vw"
+      }
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.get_scroll_y);
+  },
+  beforeDestroy() {
+    window.addEventListener('scroll', this.get_scroll_y);
+  }
+}
+</script>
+
+<style scoped>
+div.tshirt-view {
+  width: 100vw;
+  height: 55vw;
+  position: relative;
+}
+div.tshirt-view div.tshirt-view-content {
+  height: 50vw;
+  width: 70vw;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 5vw;
+  margin: auto;
+}
+div.tshirt-view div.tshirt-view-content div.tshirt-image {
+  width: 40vw;
+  height: 40vw;
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin: auto;
+  transition: all 0.5s ease-out;
+  margin-top: 2.5vw;
+  opacity: 0;
+}
+div.tshirt-view div.tshirt-view-content div.tshirt-image img {
+  width: 40vw;
+}
+div.tshirt-view div.tshirt-view-content div.tshirt-description {
+  width: 70vw;
+  height: 4vw;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 8vw;
+  margin: auto;
+  text-align: center;
+  transition: all 0.5s ease-out;
+  margin-top: 5vw;
+  opacity: 0;
+}
+div.tshirt-view div.tshirt-view-content div.tshirt-description span {
+  font-size: 2vw;
+  line-height: 4vw;
+  font-weight: bold;
+  font-family:"Yu Gothic", "游ゴシック", YuGothic, "游ゴシック体", "ヒラギノ角ゴ Pro W3", "メイリオ", sans-serif;
+}
+</style>
---
+++ components/TypingView.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="typing-view">
+    <div class="typing-view-content" id="typing">
+      <VueTyper text="Kaijo Physics Club" erase-style="clear"></VueTyper>
+    </div>
+  </div>
+</template>
+
+<script>
+import { VueTyper } from 'vue-typer';
+export default {
+  data() {
+    return {
+      scrollY: 0
+    }
+  },
+  components: {
+    VueTyper
+  },
+  methods: {
+    get_scroll_y: function () {
+      var scroll_top = window.scrollY
+      this.fade_in_out(scroll_top)
+    },
+    fade_in_out: function (scroll_top) {
+      var typing_text = document.getElementById("typing")
+
+      if (scroll_top >= (window.innerHeight * 2)) {
+        typing_text.style.opacity = 0
+      } else {
+        typing_text.style.opacity = 1
+      }
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.get_scroll_y);
+  },
+  beforeDestroy() {
+    window.addEventListener('scroll', this.get_scroll_y);
+  }
+}
+</script>
+
+<style scoped>
+@media (min-width: 600px) {
+  div.typing-view {
+    width: 100vw;
+    height: 300vh;
+    position: relative;
+  }
+  div.typing-view div.typing-view-content {
+    height: 6vw;
+    width: 80vw;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    text-align: center;
+    transition: all 0.5s ease-out;
+  }
+  div.typing-view div.typing-view-content span {
+    font-size: 3vw;
+    line-height: 6vw;
+    font-weight: bold;
+    font-family:"Yu Gothic", "游ゴシック", YuGothic, "游ゴシック体", "ヒラギノ角ゴ Pro W3", "メイリオ", sans-serif;
+  }
+}
+</style>
